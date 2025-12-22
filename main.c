66dc4232f69f67a10778962e3e29bec926793543
mohandsherief82 <219840617+mohandsherief82@users.noreply.github.com>
2025-12-22 12:26:52 +0200

    Added the 50 Move Rule

--- main.c
+++ main.c
@@ -19,8 +19,29 @@
 
 extern char *path;
 
-char playerTurn(char** board, Player* player, Captured* capture, int* plyEnPassantCol, int* opponentEnPassantCol)
+char playerTurn(char** board, Player* player, Captured* capture, int* plyEnPassantCol, int* opponentEnPassantCol, int* counter)
 {
+    if (*counter >= 100)
+    {
+        printf("Draw available by the 50-move rule!\n");
+        printf("Would you like to claim the draw? (y/n): ");
+        char choice;
+        int c;
+        
+        if (scanf(" %c", &choice) == 1) 
+        {
+            while ((c = getchar()) != '\n' && c != EOF);
+            
+            if (tolower(choice) == 'y') 
+            {
+                printf("Game drawn by the 50-move rule!\n");
+                return 't'; 
+            }
+            else printf("Continuing game...\n");
+
+        }
+    }
+
     if (checkStalemate(board, player))
     {
         printf("No Valid Moves for this player.\nGame Ended with a Stalemate!!!\n");
@@ -49,7 +70,10 @@ char playerTurn(char** board, Player* player, Captured* capture, int* plyEnPassa
         
         if (!pieceMoveValid) continue;
         
-        if (isChecked(board, player, !legalCheck)) return 'i'; 
+        if (isChecked(board, player, !legalCheck)) return 'i';
+
+        if (capture->newCapture == true || moveSymbol == 'p') *counter = 0;
+        else (*counter)++;
 
         break; 
     }
@@ -68,7 +92,7 @@ int main ()
     char** board = initializeBoard(), gameInit = '\0';
     char gameState = '_';
     int whiteEnPassantCol = -1, blackEnPassantCol = -1;
-    int c, currentPlayerTurn = 1;
+    int c, currentPlayerTurn = 1, moveCounter = 0;
 
     
     clearScreen();
@@ -124,7 +148,7 @@ int main ()
             printf("Player 1's turn: \n");
 
             isChecked(board, &ply1, false);
-            gameState = playerTurn(board, &ply1, &whiteCaptures, &whiteEnPassantCol, &blackEnPassantCol);
+            gameState = playerTurn(board, &ply1, &whiteCaptures, &whiteEnPassantCol, &blackEnPassantCol, &moveCounter);
 
             if (whiteCaptures.newCapture == true) capturePiece(&ply2, &whiteCaptures);
             
@@ -158,6 +182,7 @@ int main ()
                 printf("Game Ends!!!\nPlayer 2 wins by Resignation!!!\n");
                 break;
             }
+            else if (gameState == 't') break;
 
             clearScreen();
             updateBoard(board, ply1, ply2, whiteCaptures, blackCaptures, true);
@@ -194,7 +219,7 @@ int main ()
             printf("Player 2's turn: \n");
             
             isChecked(board, &ply2, false);
-            gameState = playerTurn(board, &ply2, &blackCaptures, &blackEnPassantCol, &whiteEnPassantCol);
+            gameState = playerTurn(board, &ply2, &blackCaptures, &blackEnPassantCol, &whiteEnPassantCol, &moveCounter);
             if (blackCaptures.newCapture == true) capturePiece(&ply1, &blackCaptures);
             
             if (gameState == 's') break;
@@ -227,6 +252,7 @@ int main ()
                 printf("Game Ends!!!\nPlayer 1 wins by Resignation!!!\n");
                 break;
             }
+            else if (gameState == 't') break;
             
             clearScreen();
             updateBoard(board, ply1, ply2, whiteCaptures, blackCaptures, true);
