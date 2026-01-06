9113f9c530d71c9c837b31cb7bd0039c63509633
mohandsherief82 <219840617+mohandsherief82@users.noreply.github.com>
2026-01-06 18:27:24 +0200

    File Renaming and Ignoring Some Files

---
+++ Pieces/include/rook.h
@@ -0,0 +1,20 @@
+#include "../../chessTypes.h"
+#include <stdbool.h>
+
+#ifndef ROCK_H
+#define ROCK_H
+
+typedef struct 
+{
+    char symbol;
+    PieceColor color;
+    int rowPosition; // Stores the row value 0 --> 7
+    int colPosition; // Stores the col value 0 --> 7
+    bool isActive;
+    bool isPinned;
+    bool firstMove;
+} Rook;
+
+Rook createRook(PieceColor color, int row, int col);
+
+#endif
