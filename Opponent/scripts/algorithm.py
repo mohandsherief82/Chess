a4c270a725e6184887cae5ea4efdac661d91209d
mohandsheried82 <219840617+mohandsherief82@users.noreply.github.com>
2026-02-04 13:30:14 +0200

    Added Initial Implementation for the Evaluation Function and the Minimax Function

--- Opponent/scripts/algorithm.py
+++ Opponent/scripts/algorithm.py
@@ -1,30 +1,34 @@
-
-piece_value = {'k':13, 'q': 9, 'r': 5, 'b': 3, 'n': 3, 'p': 1}
-
 class Minimax:
-    def __init__(self, current_state: str):
-        self.current_state = current_state
+    __piece_value = {'k':13, 'q': 9, 'r': 5, 'b': 3, 'n': 3, 'p': 1}
+    
+    def __init__(self, max_depth: int):
+        self.max_depth = max_depth
 
 
     def __actions(self):
         pass
 
     
-    def minimax(self):
-        pass
+    def minimax(self, current_state: str, depth: int = 0):
+        if self.__terminal():
+            return self.__eval(current_state)
+        
+        else:
+            pass
 
 
-    def __terminal(self):
+    def __terminal(self, current_state: str):
         pass
 
     
-    def __eval(self):
+    def __eval(self, current_state: str):
         score = 0
 
         for c in self.current_state:
-            if c.islower():
-                score = score + piece_value[c.lower()]
-            else:
-                score = score - piece_value[c.lower()]
+            if c in self.__piece_value.keys():
+                if c.islower():
+                    score = score + self.piece_value[c.lower()]
+                else:
+                    score = score - self.piece_value[c.lower()]
 
         return score
